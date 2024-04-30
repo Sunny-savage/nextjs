efc507c1b8301f03da29a5a3ac63e69e315c6988
Sunny-savage <vishal200231@gmail.com>
2024-04-30 18:53:08 +0530

    first commit

---
+++ src/models/usermodel.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Provide a name"],
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Provide an email"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Provide a password"],
+    },
+
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    forgotPasswordToken: String,
+    forgotPasswordTokenExpiry: Date,
+    verifyToken: String,
+    verfifyTokenExpiry: Date,
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+export default User;
